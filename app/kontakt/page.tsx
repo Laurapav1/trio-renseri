e55592e58c78fed402ea055e32fae64bb1d1413a
Pav <laurapav1@hotmail.com>
2024-08-17 23:35:40 +0200

    added google maps

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -62,6 +62,16 @@ export default function Kontakt() {
           </p>
         </div>
       </div>
+      <iframe
+        className={styles.googleMaps}
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2224.26985372913!2d10.196112012843631!3d56.11784987310981!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464c4042a68764c7%3A0x13b872a78fe0820f!2sTrio%20Renseri!5e0!3m2!1sda!2sus!4v1723929675858!5m2!1sda!2sus"
+        width="600"
+        height="450"
+        style={{ border: 0 }}
+        allowFullScreen
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 }
