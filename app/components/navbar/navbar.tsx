49745043810c622efca6bf05221c7c8bf6b8a804
Pav <laurapav1@hotmail.com>
2024-10-13 22:47:11 +0200

    wip

--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -1,7 +1,20 @@
+"use client";
+
+import React, { useState } from "react";
 import styles from "./navbar.module.css";
 import Link from "next/link";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev); // Improved readability
+  };
+
+  const handleLinkClick = () => {
+    setIsOpen(false); // Close the menu when a link is clicked (mobile)
+  };
+
   return (
     <nav className={styles.navbar}>
       <a href="/" className={styles.navbarLogoLink}>
@@ -11,31 +24,42 @@ export default function Navbar() {
           alt="Trio Renseri"
         />
       </a>
-      <ul className={styles.navbarList}>
+
+      {/* Hamburger Menu */}
+      <div
+        className={`${styles.hamburger} ${isOpen ? styles.active : ""}`}
+        onClick={toggleMenu}
+        aria-label="Toggle navigation menu"
+      >
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      {/* Navbar Links */}
+      <ul className={`${styles.navbarList} ${isOpen ? styles.active : ""}`}>
         <li className={styles.navbarItem}>
-          <Link href="/" passHref>
+          <Link href="/" onClick={handleLinkClick}>
             Forside
           </Link>
         </li>
         <li className={styles.navbarItem}>
-          <Link href="/services" passHref>
+          <Link href="/services" onClick={handleLinkClick}>
             Services
           </Link>
         </li>
         <li className={styles.navbarItem}>
-          <Link href="/priser" passHref>
+          <Link href="/priser" onClick={handleLinkClick}>
             Priser
           </Link>
         </li>
-
         <li className={styles.navbarItem}>
-          <Link href="/firm" passHref>
+          <Link href="/firm" onClick={handleLinkClick}>
             KundeRabat
           </Link>
         </li>
-
         <li className={styles.navbarItem}>
-          <Link href="/kontakt" passHref>
+          <Link href="/kontakt" onClick={handleLinkClick}>
             Kontakt
           </Link>
         </li>
